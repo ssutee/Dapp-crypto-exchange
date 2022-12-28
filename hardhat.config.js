4bf051944bd09aea873a34c2f9cc11d444a90a69
Millard Harris <millard@freemountaininc.com>
2022-12-27 20:29:24 -0800

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -19,5 +19,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {}
+  },
 };
