4f60aee544f120f54f75f98e88cf97ed52f2d344
Millard Harris <millard@freemountaininc.com>
2022-12-31 17:07:54 -0800

    20. Deposits pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets';
+import Balance from './Balance';
 
 function App() {
   const dispatch = useDispatch()
@@ -40,9 +42,14 @@ function App() {
 
     // Load exchange smart contract
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // Listen to events
+    subscribeToEvents(exchange, dispatch)
+
   }
 
+  
   useEffect(() => {
     loadBlockchainData()
   })
@@ -56,8 +63,8 @@ function App() {
         <section className='exchange__section--left grid'>
 
           <Markets />
-
-          {/* Balance */}
+-
+          <Balance />
 
           {/* Order */}
 
