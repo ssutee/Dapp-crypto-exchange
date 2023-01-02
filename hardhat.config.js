ddfc72908daf214292b1680a12166b1c2615edb4
Millard Harris <millard@freemountaininc.com>
2023-01-02 13:11:55 -0800

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const fs = require("fs");
+
+const privateKeys = process.env.PRIVATE_KEYS || ""
+let mnemonic = fs.readFileSync(".secret").toString().trim();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 // eslint-disable-next-line no-undef
@@ -21,6 +26,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
   },
 };
