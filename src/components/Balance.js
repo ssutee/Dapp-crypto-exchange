3ff65950c7938468f32b85f6c634d50113ac99ec
Millard Harris <millard@freemountaininc.com>
2023-01-02 11:26:47 -0800

    31. Transaction Status

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -72,8 +72,6 @@ const Balance = () => {
       transferTokens(provider, exchange, 'Withdraw', token, token2TransferAmount, dispatch)
       setToken2TransferAmount(0)
     }
-
-    console.log("withrawing tokens...")
   }
 
   useEffect(() => {
@@ -132,7 +130,7 @@ const Balance = () => {
         </div>
 
         <form onSubmit={isDeposit ? (e) => depositHandler(e, tokens[1]) : (e) => withdrawHandler(e, tokens[1])}>
-          <label htmlFor="token1"></label>
+          <label htmlFor="token1">{symbols && symbols[1]} Amount</label> 
           <input
             type="text"
             id='token1'
